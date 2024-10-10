66052b78d8d2274d9689a2f661e05fde2d2b9928
Salvador Martos <salvador.martos@gmail.com>
2024-10-10 09:59:26 +0200

    Initial commit

---
+++ app/src/main/java/com/smb/jc08_simplenavigation/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.smb.jc08_simplenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.smb.jc08_simplenavigation.ui.theme.JC08SimpleNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JC08SimpleNavigationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    /*Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )*/
+                    NavigationComplete(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JC08SimpleNavigationTheme {
+        NavigationComplete(modifier = Modifier)
+    }
+}
